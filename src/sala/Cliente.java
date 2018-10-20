8e08ce2ae9b1dd4415e94368b8487ada234d77f9
ebis19 <elu1996biscaia@gmail.com>
2018-10-20 18:35:36 -0300

    Primer msj del cliente al server

--- src/sala/Cliente.java
+++ src/sala/Cliente.java
@@ -1,9 +1,36 @@
 package sala;
 
-public class Cliente {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Cliente {
+	
+	String servidor = "localhost";
+	int puerto;
+	
+	public Cliente(String s ,int p) throws UnknownHostException , IOException{
+		
+		
+				this.servidor=s;
+				this.puerto=p;
+				 Socket so = new Socket( this.servidor,this.puerto );
+				DataInputStream input = new DataInputStream(so.getInputStream());
+				DataOutputStream  output = new DataOutputStream(so.getOutputStream());
+				output.writeUTF("hello server");
+				System.out.println(input.readUTF());
+				 
+		
+	}
+
+	
+	
+	
+	public static void main(String[] args) throws IOException{
+		
+		new Cliente("localhost",10001);
 
 	}
 
--- src/sala/Server.java
+++ src/sala/Server.java
@@ -1,9 +1,36 @@
 package sala;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int puerto;
+	ServerSocket server;
+	private Socket so;
+	private ServerSocket sc;
+	public Server(int puerto) throws IOException{
+		this.puerto =puerto;
+		sc = new ServerSocket(puerto);
+		so = new Socket();
+		so=sc.accept();
+		DataInputStream input = new DataInputStream(so.getInputStream());
+		DataOutputStream  output = new DataOutputStream(so.getOutputStream());
+		//entrada 
+		System.out.println(input.readUTF());
+		//salida
+		output.writeUTF("hello cliente");
+		
+		so.close();
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		 
+		new Server(10001);
 
 	}
 
